643563f994760ec9cea7efff715bbcae2e6195a0
Cory Chaplin <cory.chaplin@laposte.net>
2014-06-01 10:15:09 +0200

    Username plugin & module, credit @H7-25 from Simosnap.org

---
+++ server_modules/username.js
@@ -0,0 +1,15 @@
+var kiwiModules = require('../server/modules');
+ 
+var module = new kiwiModules.Module('Ident Cookie Module');
+ 
+module.on('irc connecting', function(event, event_data) {
+	var cookies = event_data.connection.state.client.websocket.transport.request ?
+				event_data.connection.state.client.websocket.transport.request.headers.cookie || '' :
+				'';
+
+	var cookie_val = cookies.match(/[ ;^]?kiwi_username=([^;]+)/);
+	
+	if (cookie_val) {
+		event_data.connection.username = cookie_val[1];
+	}
+});
