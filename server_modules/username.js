da5b7bff6e186cb87f8bd7dde7833c41f4e51955
Cory Chaplin <cory.chaplin@laposte.net>
2014-06-16 15:29:17 +0200

    Plugin fixes

--- server_modules/username.js
+++ server_modules/username.js
@@ -3,8 +3,8 @@ var kiwiModules = require('../server/modules');
 var module = new kiwiModules.Module('Ident Cookie Module');
  
 module.on('irc connecting', function(event, event_data) {
-	var cookies = event_data.connection.state.client.websocket.transport.request ?
-				event_data.connection.state.client.websocket.transport.request.headers.cookie || '' :
+	var cookies = event_data.connection.state.client.websocket.transport.req ?
+				event_data.connection.state.client.websocket.transport.req.headers.cookie || '' :
 				'';
 
 	var cookie_val = cookies.match(/[ ;^]?kiwi_username=([^;]+)/);
